c1fba2d171b92c6470d74f65d6188702fa94258c
Tommy Thach <thachtommy5@gmail.com>
2023-11-22 09:48:49 +0700

    Refactor user schema in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,37 +1,36 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, "Please enter a first name"],
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please enter a first name"],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please enter a last name"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password"],
+      minlength: [6, "Minimum password length is 6 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter an email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  lastName: {
-    type: String,
-    required: [true, "Please enter a last name"],
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please enter a password"],
-    minlength: [6, "Minimum password length is 6 characters"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please enter an email"],
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);

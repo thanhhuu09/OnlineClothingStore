fd8237d9c1f599a36760c5534fdc39337757470c
Tommy Thach <thachtommy5@gmail.com>
2023-11-26 12:31:55 +0700

    Refactor Sidebar component to use functional components and hooks

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,38 +1,42 @@
 // Sidebar component
 "use client";
 import PriceRange from "@/components/PriceRange";
-import CheckboxWithLabel from "./CheckboxWithLabel";
+import { useState } from "react";
+
 interface SidebarProps {
   onCategorySelect: (e: any) => void;
 }
 
-const categories = [
-  { id: "male", value: "men's clothing", label: "Nam" },
-  { id: "female", value: "jewelery", label: "Nữ" },
-  { id: "children", value: "children", label: "Trẻ em" },
-];
-
-const brands = [
-  { id: "adidas", value: "adidas", label: "Adidas" },
-  { id: "nike", value: "nike", label: "Nike" },
-  { id: "converse", value: "converse", label: "Converse" },
-];
-
 export default function Sidebar({ onCategorySelect }: SidebarProps) {
+  const categories = [
+    "electronics",
+    "jewelery",
+    "men's clothing",
+    "women's clothing",
+  ];
+  const brands = ["Adidas", "Nike", "Converse"];
+
+  const [priceRange, setPriceRange] = useState<number[]>([0, 100]);
+  const handlePriceRange = (e: number[]) => {
+    setPriceRange(e);
+  };
+
   return (
     <aside>
       <div className="mb-4">
         <h2 className="text-2xl font-semibold text-primary-800">Danh Mục</h2>
         <ul className="my-2">
           {categories.map((category) => (
-            <li key={category.id} className="flex items-center">
-              <CheckboxWithLabel
-                id={category.id}
-                name="category"
-                value={category.value}
-                label={category.label}
-                onCategorySelect={onCategorySelect}
+            <li key={category} className="flex items-center">
+              <input
+                className="mr-3 w-4 h-4 accent-primary-600"
+                type="checkbox"
+                name={category}
+                id={category}
+                value={category}
+                onChange={(e) => onCategorySelect(e)}
               />
+              <label htmlFor={category}>{category}</label>
             </li>
           ))}
         </ul>
@@ -41,14 +45,16 @@ export default function Sidebar({ onCategorySelect }: SidebarProps) {
         <h2 className="text-2xl font-semibold text-primary-800">Thương Hiệu</h2>
         <ul className="my-2">
           {brands.map((brand) => (
-            <li key={brand.id} className="flex items-center">
-              <CheckboxWithLabel
-                id={brand.id}
-                name="category"
-                value={brand.value}
-                label={brand.label}
-                onCategorySelect={onCategorySelect}
+            <li key={brand} className="flex items-center">
+              <input
+                className="mr-3 w-4 h-4 accent-primary-600"
+                type="checkbox"
+                name={brand}
+                id={brand}
+                value={brand}
+                onChange={(e) => onCategorySelect(e)}
               />
+              <label htmlFor={brand}>{brand}</label>
             </li>
           ))}
         </ul>
@@ -56,10 +62,11 @@ export default function Sidebar({ onCategorySelect }: SidebarProps) {
       <div>
         <h2 className="text-2xl font-semibold text-primary-800">Giá</h2>
         <ul className="my-2">
-          <PriceRange />
+          <PriceRange priceRange={handlePriceRange} />
           <li className="flex items-center">
             <p className="font-medium text-lg">
-              0 - 100 <span className="text-primary-700">đ</span>
+              {priceRange[0]} - {priceRange[1]}{" "}
+              <span className="text-primary-700">đ</span>
             </p>
           </li>
         </ul>

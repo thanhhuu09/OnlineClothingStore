b20bfb9c14ac33caba43f6348d6a9b25868ccecd
Tommy Thach <thachtommy5@gmail.com>
2023-11-28 15:50:26 +0700

    Refactor SizeBox component styling

--- client/src/components/SizeBox.tsx
+++ client/src/components/SizeBox.tsx
@@ -17,18 +17,21 @@ export default function SizeBox({
     }
   };
   return (
+    // Box for size
     <div>
       <div
         onClick={handleOnClick}
-        className={`w-8 h-8 rounded border border-solid flex justify-center items-center text-center ${
-          isSelected && "bg-secondary-500 "
-        }}`}
+        className={`
+            w-8 h-8 rounded border cursor-pointer border-solid flex justify-center items-center text-center 
+            text-base p-2 transition-colors duration-200 ease-in-out
+            ${
+              isSelected
+                ? "bg-secondary-500 text-secondary-50"
+                : "text-primary-900 hover:bg-primary-100 focus:bg-primary-100"
+            }
+          `}
       >
-        <p
-          className={`${isSelected ? "text-secondary-50" : "text-primary-900"}`}
-        >
-          {size}
-        </p>
+        {size}
       </div>
     </div>
   );

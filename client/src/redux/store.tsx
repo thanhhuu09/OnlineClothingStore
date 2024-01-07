f8c6d9c877f53d5c4b062386a73a8cad187e227e
Tommy Thach <thachtommy5@gmail.com>
2024-01-07 13:45:58 +0700

    Refactor redux store configuration

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,44 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { TypedUseSelectorHook, useSelector } from "react-redux";
-// import cartReducer from "@/redux/features/cartSlice";
-// import authReducer from "@/redux/features/authSlice";
-// // Redux Persist
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-
-// const persistConfig = {
-//   key: "root",
-//   version: 1,
-//   storage,
-// };
-// const rootReducer = combineReducers({
-//   cart: cartReducer,
-//   auth: authReducer,
-// });
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// let persistor = persistStore(store);
-// export { persistor, store };
-
 import cartReducer from "@/redux/features/cartSlice";
 import authReducer from "@/redux/features/authSlice";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
